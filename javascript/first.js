c0878dee26fd21b197dd2e9f25b8d1d7048a4708
RuthvikSai2015 <84248537+RuthvikSai2015@users.noreply.github.com>
2023-12-19 18:23:51 +0530

    added javascript

--- javascript/first.js
+++ javascript/first.js
@@ -1,2 +1,17 @@
 console.log("First Javascript Program"); //printing on console
-document.getElementById("paragraph").innerHTML="First Paragraph";
+// document.getElementById("paragraph").innerHTML="First Paragraph";
+
+var fruit = "Apple";
+//console.log("data type------",typeof(fruit));
+
+var x;
+console.log(typeof(x));  // 10
+var x=10;
+console.log(x); // 
+console.log(typeof(x)); // 
+x=9.6;
+console.log(typeof(x)); //
+x=false;
+console.log(typeof(x)); //
+x="savitha";
+console.log(typeof(x)); //
