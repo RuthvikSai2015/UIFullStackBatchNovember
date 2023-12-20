833f267f254babee776eee3d56d85db68a574134
RuthvikSai2015 <84248537+RuthvikSai2015@users.noreply.github.com>
2023-12-20 18:12:27 +0530

    added javascript

--- javascript/first.js
+++ javascript/first.js
@@ -5,13 +5,13 @@ var fruit = "Apple";
 //console.log("data type------",typeof(fruit));
 
 var x;
-console.log(typeof(x));  // 10
+console.log(typeof(x));  // undefined
 var x=10;
-console.log(x); // 
-console.log(typeof(x)); // 
+console.log(x); // 10
+console.log(typeof(x)); // number
 x=9.6;
-console.log(typeof(x)); //
+console.log(typeof(x)); // float
 x=false;
-console.log(typeof(x)); //
+console.log(typeof(x)); // boolean
 x="savitha";
-console.log(typeof(x)); //
+console.log(typeof(x)); // string
