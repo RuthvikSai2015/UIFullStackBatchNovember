124a6a8a9ba14e40c847933c0c322d9b223c917f
RuthvikSai2015 <84248537+RuthvikSai2015@users.noreply.github.com>
2023-12-21 18:47:54 +0530

    added javascript

--- javascript/first.js
+++ javascript/first.js
@@ -4,14 +4,32 @@ console.log("First Javascript Program"); //printing on console
 var fruit = "Apple";
 //console.log("data type------",typeof(fruit));
 
-var x;
-console.log(typeof(x));  // undefined
-var x=10;
-console.log(x); // 10
-console.log(typeof(x)); // number
-x=9.6;
-console.log(typeof(x)); // float
-x=false;
-console.log(typeof(x)); // boolean
-x="savitha";
-console.log(typeof(x)); // string
+// var x;
+// console.log(typeof(x));  // undefined
+// var x=10;
+// console.log(x); // 10
+// console.log(typeof(x)); // number
+// x=9.6;
+// console.log(typeof(x)); // float
+// x=false;
+// console.log(typeof(x)); // boolean
+// x="savitha";
+// console.log(typeof(x)); // string
+
+var b1=true , b2 = false, b3=true;
+	
+	    console.log(b1 && b2) ;  // false 
+		console.log(b1 && b3) ;  // truw
+		console.log(b1 && b2 && b3);//left to right output false 
+		console.log(b1 || b2); //true
+		console.log(b1 || b3); //true
+		console.log(b1 || b2 || b3); //true 
+		console.log(b1 || b2 && b3); //true
+		// console.log(b1 || b2); // true 
+		// console.log(!b3); // false 
+        console.log(b2 && !b3);
+        console.log(b1 || b2 && !b3); // worst case
+                    //  true && false => true 
+
+                    
+	
