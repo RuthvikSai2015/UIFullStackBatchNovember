26c7b1115cea4b83a73774d2868088e759ac7784
RuthvikSai2015 <84248537+RuthvikSai2015@users.noreply.github.com>
2023-12-27 20:27:14 +0530

    added javascript

---
+++ javascript/loops.js
@@ -0,0 +1,17 @@
+// for(var i=1;i<=10;i++){
+
+//     if(i==5)
+//       break;
+//    else
+//      console.log(i);
+ 
+//  }
+//  console.log("after loop");
+ for(var j=1;j<=10;j++){
+
+    if(j==5)
+      continue;
+   else
+     console.log(j);
+ 
+ }
---
+++ javascript/switch.js
@@ -0,0 +1,29 @@
+var userChoice=2;
+switch(userChoice){
+    case 1: console.log('AC');
+            break;
+    case 2: console.log('Fan');
+           break;
+    case 3:console.log('Inverter');
+           break;
+    default:console.log('None of them selected');
+           break;
+}
+// One more example
+// + , -  , / , * num1 num2
+// var i=1,result='';
+// while(i<100){
+//     //result += i + ' ';
+//     process.stdout.write(i+' ');
+//     i+=2;
+// }
+//console.log(result);
+var i=1;
+// do{
+//     console.log(i);
+//     i+=2;
+// }while(i<1);
+
+for(i=1;i<=10;i++){
+    console.log(i);
+}
