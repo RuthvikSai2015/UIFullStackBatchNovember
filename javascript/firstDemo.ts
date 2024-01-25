11cd2b147bc6245de11bd03e1da98a181713a693
RuthvikSai2015 <84248537+RuthvikSai2015@users.noreply.github.com>
2024-01-25 18:18:57 +0530

    Typescript

---
+++ javascript/firstDemo.ts
@@ -0,0 +1,20 @@
+console.log("first demo");
+
+let age:number = 25;
+let firstName:string = "savitha";
+
+age = 56;
+
+let familyAges:number[] = [21,34,56,78];
+		  
+let person:object = { personAge:21,personName:'Ajay'};
+
+//let x:number = "Apple";
+
+let x:any = "Apple";
+
+function showMessage():number{
+
+    return 4;
+}
+showMessage();
